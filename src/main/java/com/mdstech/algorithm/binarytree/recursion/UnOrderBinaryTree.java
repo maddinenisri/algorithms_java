3f5ced4ec4fff29d8bbd673bf7f22e49422f739c
Srini Maddineni <maddinenisri@gmail.com>
2015-08-17 15:48:08 -0400

    Update UnOrderBinaryTree.java

--- src/main/java/com/mdstech/algorithm/binarytree/recursion/UnOrderBinaryTree.java
+++ src/main/java/com/mdstech/algorithm/binarytree/recursion/UnOrderBinaryTree.java
@@ -18,6 +18,47 @@ public class UnOrderBinaryTree {
 			addRecursiveTreeNode(rootNode, index, name);
 		}
 	}
+	/**
+	 * InOrder - Left --> Parent --> Right
+	 */
+	public void inOrderTraversal(TreeNode treeNode, StringBuffer sb) {
+		if(treeNode.getLeftNode() != null) {
+			inOrderTraversal(treeNode.getLeftNode(), sb);
+		}
+		sb.append(treeNode.getName()+"("+treeNode.getKey()+") -->");
+
+		if(treeNode.getRightNode() != null) {
+			inOrderTraversal(treeNode.getRightNode(), sb);
+		}
+	}
+	
+	/**
+	 * InOrder - Parent --> Left --> Right
+	 */
+	public void preOrderTraversal(TreeNode treeNode, StringBuffer sb) {
+		sb.append(treeNode.getName()+"("+treeNode.getKey()+") -->");
+
+		if(treeNode.getLeftNode() != null) {
+			preOrderTraversal(treeNode.getLeftNode(), sb);
+		}
+		if(treeNode.getRightNode() != null) {
+			preOrderTraversal(treeNode.getRightNode(), sb);
+		}
+	}
+
+	/**
+	 * InOrder - Left --> Right --> Parent
+	 */
+	public void postOrderTraversal(TreeNode treeNode, StringBuffer sb) {
+		if(treeNode.getLeftNode() != null) {
+			postOrderTraversal(treeNode.getLeftNode(), sb);
+		}
+
+		if(treeNode.getRightNode() != null) {
+			postOrderTraversal(treeNode.getRightNode(), sb);
+		}
+		sb.append(treeNode.getName()+"("+treeNode.getKey()+") -->");
+	}
 
 	private void addRecursiveTreeNode(TreeNode parentNode, Integer index, String name) {
 		boolean isNodeInserted = false;
