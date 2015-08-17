33104d6876f6165d2550d92a4b4ea1d2d80d8a82
Srini Maddineni <maddinenisri@gmail.com>
2015-08-17 14:57:48 -0400

    Update UnOrderBinaryTree.java
    Implemented Queue Strategy for Balanced Unorder Tree Structure

--- src/main/java/com/mdstech/algorithm/binarytree/recursion/UnOrderBinaryTree.java
+++ src/main/java/com/mdstech/algorithm/binarytree/recursion/UnOrderBinaryTree.java
@@ -1,13 +1,18 @@
 package com.mdstech.algorithm.binarytree.recursion;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class UnOrderBinaryTree {
 
 	private TreeNode rootNode;
+	private Queue<TreeNode> treeQueue = new LinkedList<TreeNode>();
 	
 	public void addTreeNode(Integer index, String name) {
 		//If root node is null, create root node
 		if(rootNode == null) {
 			rootNode = new TreeNode(index, name);
+			treeQueue.add(root);
 		}
 		else {
 			addRecursiveTreeNode(rootNode, index, name);
@@ -19,15 +24,26 @@ public class UnOrderBinaryTree {
 		if(parentNode.getLeftNode() == null) {
 			parentNode.setLeftNode(new TreeNode(index, name));
 			isNodeInserted = true;
+			treeQueue.add(parentNode.getLeftNode());
 		}
 		else if(parentNode.getRightNode() == null) {
 			parentNode.setRightNode(new TreeNode(index, name));
 			isNodeInserted = true;
+			parentNode.setFullNode(true);
+			treeQueue.add(parentNode.getRightNode());
 		}
 		
 		if(!isNodeInserted) {
-			addRecursiveTreeNode(findParetNodeToAddNode(parentNode), index, name);
+			addRecursiveTreeNode(findParetNodeToAddNode(), index, name);
 		}
+	}
+	
+	private TreeNode findParetNodeToAddNode() {
+		TreeNode parentNode = treeQueue.element();
+		if(treeQueue.element().isFullNode()) {
+			treeQueue.remove();
+		}
+		return parentNode;
 	}
 	
 	private TreeNode findParetNodeToAddNode(TreeNode currentMarkerNode) {
